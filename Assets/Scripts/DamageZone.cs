531039cb03d7f86e1b1c3b816a8f27bda8979405
Nicola Gernone <nicola.gernone02@gmail.com>
2021-02-06 12:19:25 +0100

    Animation and animator

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+                controller.ChangeHealth(-1);
+            
+        }
+    }
     void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -17,6 +17,9 @@ public class RubyController : MonoBehaviour
     new Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,7 @@ public class RubyController : MonoBehaviour
         //Application.targetFrameRate = 10;
         rigidbody2D = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
     }
 
@@ -32,6 +36,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -56,6 +72,8 @@ public class RubyController : MonoBehaviour
 
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+            
             if(isInvincible)
                 return;
                 
